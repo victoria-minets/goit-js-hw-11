07ec4da12009ff56af23bf1c30640226f956c5f2
Victoria Minets <anni.nicht@gmail.com>
2025-07-23 17:38:33 +0300

    add structure

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// import fetchData from './js/pixabay-api';
+// import moduleName from './js/render-functions';
+
+// У файлі main.js напиши всю логіку роботи додатка.Виклики нотифікацій
+// iziToast, усі перевірки на довжину масиву в отриманій відповіді робимо
+// саме в цьому файлі.Імпортуй в нього функції із файлів pixabay - api.js
+// та render - functions.js та викликай їх у відповідний момент.
